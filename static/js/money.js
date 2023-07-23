2f186b5cd988f0a13d146d06b5f4fd2e78bf6fc7
saban <sabann248@gmail.com>
2023-07-23 21:19:11 +0300

    0.3.5 - new feature, fixed

---
+++ static/js/money.js
@@ -0,0 +1,20 @@
+
+
+/**
+ * 
+ * @param {Element} t 
+ */
+function filter_date(t){
+
+}
+
+
+// ----------------- charts ---------------------
+
+function setLabelChartMoeny(_date_list){
+    parent = document.getElementById('footer-labels')
+    for (d of _date_list){
+        console.log(d)
+    }
+
+}
